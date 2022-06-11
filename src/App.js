2ebf2fcc9baf4a959e7876fd9d437108d76f9ea4
Dino Hadzic <zgdino@yahoo.com>
2022-06-11 17:14:35 +0200

    error route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <Router>
-       
+      {/* switch renders the first route that matches; using it to avoid displaying multiple pages at the same time */}
       <Switch>
         {/* Dashboard is our homepage. Making sure the url matches exactly */}
         <Route path='/' exact={true}>
