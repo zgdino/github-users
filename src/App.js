c58a3d9629fedb4ac7b071f387cb0cdae97b7222
Dino Hadzic <zgdino@yahoo.com>
2022-06-11 16:56:02 +0200

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
-  );
+    <Router>
+      {/* Dashboard is our homepage */}
+      <Route path='/'>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path='/login'>
+        <Login></Login>
+      </Route>
+      {/* <Error /> */}
+    </Router>
+  )
 }
 
-export default App;
+export default App
