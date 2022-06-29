299a7c0313da52e794addf7b086ee79fea2a8402
Dino Hadzic <zgdino@yahoo.com>
2022-06-29 20:04:56 +0100

    Promise allSettled

--- src/context/context.js
+++ src/context/context.js
@@ -40,17 +40,19 @@ const GithubProvider = ({ children }) => {
       await Promise.allSettled([
         axios(`${rootUrl}/users/${login}/repos?per_page=100`),
         axios(`${followers_url}?per_page=100`),
-      ]).then((results) => {
-        console.log(results)
-        const [repos, followers] = results
-        const status = 'fulfilled'
-        if (repos.status === status) {
-          setRepos(repos.value.data)
-        }
-        if (followers.status === status) {
-          setFollowers(followers.value.data)
-        }
-      })
+      ])
+        .then((results) => {
+          console.log(results)
+          const [repos, followers] = results
+          const status = 'fulfilled'
+          if (repos.status === status) {
+            setRepos(repos.value.data)
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data)
+          }
+        })
+        .catch((err) => console.log(err))
     } else {
       toggleError(true, 'invalid username')
     }
