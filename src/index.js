77753562bf5c7ba9ed7cd48969033d7390da8f89
Dino Hadzic <zgdino@yahoo.com>
2022-06-30 11:13:58 +0100

    auth0 setup

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ import * as serviceWorker from './serviceWorker'
 import { GithubProvider } from './context/context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+// if necessary, set a .env variables to hide them from public
+// DOMAIN
+// dev-oeebqrcp.us.auth0.com
+// CLIENT ID
+// nUP3elnhoOV266IypdP33dZqy88cm334
+
 ReactDOM.render(
-  <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+  <React.StrictMode
+    domain='dev-oeebqrcp.us.auth0.com'
+    clientId='nUP3elnhoOV266IypdP33dZqy88cm334'
+    redirectUri={window.location.origin}
+  >
+    <Auth0Provider>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
