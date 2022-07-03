f555aae1f9dd0b0e52e4971ae9ff278ac9a7b407
Dino Hadzic <zgdino@yahoo.com>
2022-07-03 12:46:59 +0100

    Setup the AuthWrapper.js component

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import loadingGif from '../images/preloader.gif';
 import styled from 'styled-components';
-function AuthWrapper() {
-  return <h2>authwrapper component</h2>;
+function AuthWrapper({children}) {
+  if (loading) {
+    return <Wrapper>
+      <img src={loadingGif} alt="spinner" />
+    </Wrapper>
+  }
+  if (error) {
+    return <Wrapper><h1>{error.message}</h1></Wrapper>
+  }
+  // children are all of the components wrapped in AuthWrapper - check App.js for reference
+  return <>{children}</>;
 }
 
 const Wrapper = styled.section`
