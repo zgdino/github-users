81098195654f678cc9792a082fa02d82511a3c6b
Dino Hadzic <zgdino@yahoo.com>
2022-07-03 12:37:42 +0100

    Wrap the Router in App.js into AuthWrapper
    Show dashboard if login has been succesful

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      {/* switch renders the first route that matches; using it to avoid displaying multiple pages at the same time */}
-      <Switch>
-        {/* Dashboard is our homepage. Making sure the url matches exactly */}
-        <PrivateRoute path='/' exact={true}>
-          <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route path='/login'>
-          <Login></Login>
-        </Route>
-        {/* sign '*' refers to the page that does not exist */}
-        <Route path='*'>
-          <Error></Error>
-        </Route>
-      </Switch>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        {/* switch renders the first route that matches; using it to avoid displaying multiple pages at the same time */}
+        <Switch>
+          {/* Dashboard is our homepage. Making sure the url matches exactly */}
+          <PrivateRoute path='/' exact={true}>
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path='/login'>
+            <Login></Login>
+          </Route>
+          {/* sign '*' refers to the page that does not exist */}
+          <Route path='*'>
+            <Error></Error>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
   )
 }
 
