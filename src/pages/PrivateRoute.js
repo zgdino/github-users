a201d3722c2d5dd6d5f09425613aa6ec14375f37
Dino Hadzic <zgdino@yahoo.com>
2022-07-02 23:14:08 -1200

    Tie local variable at PrivateRoute.js to a global one

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,7 +3,9 @@ import { Route, Redirect } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const isUser = false
+  const {isAuthenticated, user} = useAuth0()
+  // local variable isUser directly tied to following two
+  const isUser = isAuthenticated && user
   return (
     <Route
       {...rest}
