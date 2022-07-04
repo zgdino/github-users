af26485335b7f090e07e7d169adbebfb6faa50d1
Dino Hadzic <zgdino@yahoo.com>
2022-07-04 17:01:22 +0100

    Refactor to react router 6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     // whatever is wrapped in AuthWrapper becomes it children, that same way Router children is everything wrapped in it
     <AuthWrapper>
       <Router>
-        {/* switch renders the first route that matches; using it to avoid displaying multiple pages at the same time */}
-        <Switch>
-          {/* Dashboard is our homepage. Making sure the url matches exactly */}
-          <PrivateRoute path='/' exact={true}>
-            <Dashboard></Dashboard>
-          </PrivateRoute>
-          <Route path='/login'>
-            <Login></Login>
-          </Route>
-          {/* sign '*' refers to the page that does not exist */}
-          <Route path='*'>
-            <Error></Error>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
       </Router>
     </AuthWrapper>
   )
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,19 +1,7 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
-const PrivateRoute = ({ children, ...rest }) => {
-  const {isAuthenticated, user} = useAuth0()
-  // local variable isUser directly tied to following two
-  const isUser = isAuthenticated && user
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        // if isUser is true return children(dashboard in this case - look into App.js), else redirect to login page
-        return isUser ? children : <Redirect to='/login'></Redirect>
-      }}
-    ></Route>
-  )
+const PrivateRoute = () => {
+  return <h2>private route</h2>
 }
 export default PrivateRoute
